1da6d056a51190d039edef22b3c6876b0b20d6a1
Brian Bycynski <high5bri@hotmail.com>
2022-02-12 04:12:26 -0500

    meme generator

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,12 +3,20 @@
 
 import React from 'react';
 
+import memesData from '../memeData';
 import '../styles/Meme.css';
 
 export default function Meme() {
+  function getNewImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    console.log(url);
+  }
+
   return (
     <main className='meme-main'>
-      <form className='meme-form'>
+      <div className='meme-form'>
         <input
           type='text'
           className='meme-form--input'
@@ -19,8 +27,10 @@ export default function Meme() {
           className='meme-form--input'
           placeholder='Bottom Text'
         />
-        <button className='meme-form--button'>Get a new meme image</button>
-      </form>
+        <button onClick={getNewImage} className='meme-form--button'>
+          Get a new meme image
+        </button>
+      </div>
     </main>
   );
 }
