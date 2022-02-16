ded852910a15197b76e85a864a17013fad275e5d
Brian Bycynski <high5bri@hotmail.com>
2022-02-16 08:51:00 -0500

    meme component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,15 @@
 
 import React from 'react';
 
+import logoImage from '../images/troll-face.png';
+
 import '../styles/Header.css';
 
 export default function Header() {
   return (
     <header className='header'>
-      <img className='' src='../images/troll-face.png' alt='' />
-      <h1 className=''>Meme Generator</h1>
+      <img className='header--image' src={logoImage} alt='' />
+      <h1 className='header--title'>Meme Generator</h1>
     </header>
   );
 }
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,13 +2,37 @@
 //
 
 import React from 'react';
-
 import '../styles/Meme.css';
+import { memesData } from '../memesData';
+console.log(memesData);
 
 export default function Meme() {
+  let url;
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    url = memesArray[randomNumber].url;
+    console.log(url);
+  }
+
   return (
-    <div className='meme'>
-      <h1>Meme</h1>
-    </div>
+    <main className='meme'>
+      <div className='meme--form'>
+        <input
+          type='text'
+          placeholder='Top Text'
+          className='meme--form-input'
+        />
+        <input
+          type='text'
+          placeholder='Bottom Text'
+          className='meme--form-input'
+        />
+        <button onClick={getMemeImage} className='meme--form-button'>
+          Get a new meme Image
+        </button>
+        <img src={url} alt='' />
+      </div>
+    </main>
   );
 }
