3043d08322359e0d06a9e9075e80bb03da440a81
Brian Bycynski <high5bri@hotmail.com>
2022-02-18 03:48:37 -0500

    Update Meme.js

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -5,15 +5,26 @@ import React from 'react';
 import '../styles/Meme.css';
 import { memesData } from '../memesData';
 
-console.log(memesData);
-
 export default function Meme() {
-  const [memeImage, setMemeImage] = React.useState('');
+  const [meme, setMeme] = React.useState({
+    topText: '',
+    bottomText: '',
+    randomImage: 'http://i.imgflip.com/1bij.jpg',
+  });
+
+  /* eslint-disable no-unused-vars */
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData);
 
   function getMemeImage() {
-    const memesArray = memesData.data.memes;
+    const memesArray = allMemeImages.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
-    setMemeImage(memesArray[randomNumber].url);
+    const url = memesArray[randomNumber].url;
+    setMeme((prevMeme) => {
+      return {
+        ...prevMeme,
+        randomImage: url,
+      };
+    });
   }
 
   return (
@@ -32,7 +43,7 @@ export default function Meme() {
         <button onClick={getMemeImage} className='meme--form-button'>
           New Image
         </button>
-        <img className='meme--form-image' src={memeImage} alt='' />
+        <img className='meme--form-image' src={meme.randomImage} alt='' />
       </div>
     </main>
   );
